44582f0090bbbaf7c9ff2629088975ce62e2e547
Henrique Paschero Cima <129294791+henrique-cima@users.noreply.github.com>
2024-08-15 08:34:05 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+console.log("Script Carregado");
+function calcularAnos(){
+console.log("botão de Calcular clicando");
+let popAInput = document.getElementById("popA").value.trim;
+let taxaAInput = document.getElementById("taxaA").value.trim;
+let popBInput = document.getElementById("popB").value.trim;
+let taxaBInput = document.getElementById("taxaB").value.trim;
+}
